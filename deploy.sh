cc753b47a32a3f86d31216df364255807a6e34fe
kssk4123 <ken16.ssk@gmail.com>
2025-01-06 19:08:57 +0900

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@ APP_NAME="app"
 BUILD_DIR="/opt/goapp/src"
 LOG_DIR="/opt/goapp/logs"
 SRC_DIR="/opt/goapp/src"
-MAX_BACKUPS=1  # 残すバイナリの最大数
-MAX_LOGS=10  # 残すログファイルの最大数
+# MAX_BACKUPS=1  # 残すバイナリの最大数
+# MAX_LOGS=10  # 残すログファイルの最大数
 
 # バイナリ名を一意化 (例: 日時付き)
-NEW_BINARY="$BUILD_DIR/$APP_NAME-$(date +%Y%m%d%H%M%S)"
+# NEW_BINARY="$BUILD_DIR/$APP_NAME-$(date +%Y%m%d%H%M%S)"
 
 # 最新コード取得
 cd "$SRC_DIR"
@@ -25,7 +25,7 @@ go build -o "$APP_NAME" .
 # ls -1t "$BUILD_DIR/$APP_NAME-"* | tail -n +$((MAX_BACKUPS + 1)) | xargs -r rm -f
 
 # 古いログファイルを削除
-ls -1t "$LOG_DIR/app.log.*" | tail -n +$((MAX_LOGS + 1)) | xargs -r rm -f
+# ls -1t "$LOG_DIR/app.log.*" | tail -n +$((MAX_LOGS + 1)) | xargs -r rm -f
 
 # サービス再起動
 sudo systemctl restart myapp.service
